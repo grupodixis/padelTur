450df6cfb8d5e8554012229b1a3853e4b26d7f84
Pablo  S. Sanchez Arnoletto <pablo@dixis.es>
2018-02-05 12:43:08 +0100

    Actualizacion de Modelos

--- data.ts
+++ data.ts
@@ -1,16 +1,50 @@
-export interface Partido {
-idPartido: number; // Identificador unico del partido
-idGrupo: number; // grupo id 4 Indentificamos a que grupo pertenece este partido
-idEquipos: number [] ; // [1,2] Identificamos a los equipos que juegan este partido
-resultado: number []; // [[6,4],[4,6],[6,2]]
+export interface Circuito{
+  idCircuito: Number;  //Identificador unico del circuto 
+  nombreCircuito: String;  // Nombre del Circuito  Ej: Menorca padel tour
+
+}
+export interface Temporada{
+  idTemporada: Number;  // Identificador de temporada
+  NombreTemporada: String; // Nombre de la temporada Ej: Temporada 2018/2019
+  idCircuito: Number;   //Identifica a que circuito pertenece
+}
+
+export interface Ranking{
+  nombre: String;  // Por ejemplo: Categoria 4º
+  idRanking: Number; // Identificador unico de Ranking
+  idTemporada: Number;  //Identificador  
+}
+
+export interface Torneo{
+  idTorneo: Number;  // Identificador del torneo
+  nombreTorneo: String;  // Nombre del torneo
+  idRanking: Number;  //Identifico en que ranking puntua
+
 }
 
 export interface Grupo {
   idGrupo: Number;  // id : 1 // Identificador unico del grupo
-  equipos: Number [];   // [1, 2, 3, 4] //equipos que participan en el grupo ordenados por ranking.
+  nombreGrupo: String; // Nombre del grupo Ej: Grupo A, Grupo B etc.
+  idTorneo:Number;  // Identificador del torneo al que pertenece el grupo.
   puntos: Number; // Cantidad de puntos que suma un jugador por estar aqui
-  partidos: Number []; //id de partidos que se juegan en este grupo
+  
 }
+
+export interface Playoff {
+  idPlayoff: Number; // id: 1
+  idTorneo: Number;  // Asociacion al torneo qe
+  partida: Number; //  32 u 16 o 8  -> Si se parte de Octavos o cuartos
+  Equipos: Number [];  // array de ids de equipos en orden de triunfo
+  puntos: Number; // Cantidad de puntos que gana el jugador hasta aqui.
+}
+
+export interface Partido {
+  idPartido: number; // Identificador unico del partido
+  infoPartido: {nombre:String,Fecha:Date}; // Ejemplo Nombre: 1º Partido Fecha: 12/12/2019 20:30 hs
+  idGrupo: number; // grupo id 4 Indentificamos a que grupo pertenece este partido
+  resultado: [{ idEquipo1: Number, sets: Number [] }]  //  Resultados [{ idEquipo1: 2, sets: Number [6,4,3] },{ idEquipo1: 3, sets: Number [4,6,6] }]
+  }
+
 
 export interface Equipo {
   idEquipo: Number;  // id : 1 Identificador unico del Equipo
@@ -26,13 +60,6 @@ export interface Jugador {
   PuntosRanking: Number []; // [4,100] [3,300] -> Suma de puntos ranking por nivel
 }
 
-export interface Playoff {
-  idNivel: Number; // Asociamos al nivel que pertenece
-  idPlayoff: Number; // id: 1
-  partida: Number; //  32 u 16 o 8  -> Si se parte de Octavos o cuartos
-  Equipos: Number [];  // array de ids de equipos en orden de triunfo
-  puntos: Number; // Cantidad de puntos que gana el jugador hasta aqui.
-}
 
 export interface Nivel {
   idNivel: Number; // identificador unico del nivel que se juega en ese torneo ej 32.
@@ -40,16 +67,9 @@ export interface Nivel {
   nivel: Number;  // Nivel real en cuestion que permitira a los jugadores entrar ej 4
 }
 
-export interface Torneo{
-  idTorneo: Number;  // Identificador del torneo
-  nombreTorneo: String;  // Nombre del torneo
-}
 
-export interface Ranking{
-  season: String;  // Temporada 2018 /2019 
-  idTorneos: Number [];  // Array con los id de torneos que formaran parte de esa temporada
 
-}
+
 
 /*
 Dado el siguiente ranking de jugadores:
